25d0e362f74453754c0225ceac1eb2ae895e28a1
Anran Zhang <Thansy@foxmail.com>
2021-07-27 17:33:08 +0800

    添加气泡

---
+++ src/App/Controls/BubbleView.Properties.cs
@@ -0,0 +1,59 @@
+﻿// Copyright (c) Richasy. All rights reserved.
+
+using Microsoft.Graphics.Canvas;
+using System.Collections.Generic;
+using Windows.UI;
+using Windows.UI.Composition;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Shapes;
+
+namespace Richasy.Bili.App.Controls
+{
+    /// <summary>
+    /// 气泡发生器.
+    /// </summary>
+    public partial class BubbleView
+    {
+        /// <summary>
+        /// <see cref="IsBubbing"/>的依赖属性.
+        /// </summary>
+        public static readonly DependencyProperty IsBubbingProperty =
+            DependencyProperty.Register(nameof(IsBubbing), typeof(bool), typeof(BubbleView), new PropertyMetadata(false, (s, a) =>
+            {
+                if (a.NewValue != a.OldValue)
+                {
+                    if (s is BubbleView sender)
+                    {
+                        if (a.NewValue is true)
+                        {
+                            sender.ShowBubbles();
+                            sender.SetValue(IsBubbingProperty, false);
+                        }
+                    }
+                }
+            }));
+
+        private Rectangle _bubbleHost;
+        private Color _foregroundColor;
+
+        private Compositor _compositor;
+        private Visual _hostVisual;
+        private ContainerVisual _bubblesVisual;
+
+        private CanvasDevice _canvasDevice;
+        private CompositionGraphicsDevice _graphicsDevice;
+
+        private List<Bubble> _bubbles;
+
+        private long _foregroundPropertyChangedToken;
+
+        /// <summary>
+        /// 当设置IsBubbing = true时，触发ShowBubbles，并将IsBubbing设置为false，等待下次设置IsBubbing = true.
+        /// </summary>
+        public bool IsBubbing
+        {
+            get { return (bool)GetValue(IsBubbingProperty); }
+            set { SetValue(IsBubbingProperty, value); }
+        }
+    }
+}
---
+++ src/App/Controls/VisualExtensions.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Richasy. All rights reserved.
+
+using Windows.UI.Composition;
+
+namespace Richasy.Bili.App.Controls
+{
+    /// <summary>
+    /// 视觉扩展.
+    /// </summary>
+    public static class VisualExtensions
+    {
+        /// <summary>
+        /// 绑定大小.
+        /// </summary>
+        /// <param name="target">目标元素.</param>
+        /// <param name="source">来源视图.</param>
+        public static void BindSize(this Visual target, Visual source)
+        {
+            var exp = target.Compositor.CreateExpressionAnimation("host.Size");
+            exp.SetReferenceParameter("host", source);
+            target.StartAnimation("Size", exp);
+        }
+
+        /// <summary>
+        /// 绑定中心点.
+        /// </summary>
+        /// <param name="target">目标视图.</param>
+        public static void BindCenterPoint(this Visual target)
+        {
+            var exp = target.Compositor.CreateExpressionAnimation("Vector3(this.Target.Size.X / 2, this.Target.Size.Y / 2, 0f)");
+            target.StartAnimation("CenterPoint", exp);
+        }
+    }
+}
