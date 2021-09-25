f15a10fa240155c9ea4c3610b9f1d6424604036c
Richasy <Thansy@foxmail.com>
2021-09-25 09:32:59 +0800

    支持修改双击播放区的行为 (#290)

---
+++ src/Models/Models.Enums/App/DoubleClickBehavior.cs
@@ -0,0 +1,20 @@
+﻿// Copyright (c) Richasy. All rights reserved.
+
+namespace Richasy.Bili.Models.Enums
+{
+    /// <summary>
+    /// 双击播放器的行为.
+    /// </summary>
+    public enum DoubleClickBehavior
+    {
+        /// <summary>
+        /// 全屏.
+        /// </summary>
+        FullScreen,
+
+        /// <summary>
+        /// 播放/暂停.
+        /// </summary>
+        PlayPause,
+    }
+}
