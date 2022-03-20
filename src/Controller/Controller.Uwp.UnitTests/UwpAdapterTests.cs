eafbee4a6e55a7a2b6fe61d7e2a711cdaae8f439
Richasy <Thansy@foxmail.com>
2022-03-20 21:52:50 +0800

    更新ffmpeg以更好地支持HDR和AV1 (#863)

--- src/Controller/Controller.Uwp.UnitTests/UwpAdapterTests.cs
+++ src/Controller/Controller.Uwp.UnitTests/UwpAdapterTests.cs
@@ -1,16 +1,8 @@
 ﻿// Copyright (c) Richasy. All rights reserved.
 
-using FluentAssertions;
-using Xunit;
-
 namespace Controller.Uwp.UnitTests
 {
     public class UwpAdapterTests
     {
-        [Fact]
-        public void SampleTest()
-        {
-            true.Should().BeFalse();
-        }
     }
 }
