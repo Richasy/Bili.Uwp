0f677fdf79283c57f2c1d97b64e7b8aea2f0e474
Richasy <Thansy@foxmail.com>
2023-04-11 15:40:04 +0800

    给哔哩空间添加开机启动 (#1676)
    * 添加开机启动
    
    * Update references

--- src/ViewModels/ViewModels.Workspace/Core/SettingsViewModel/SettingsViewModel.cs
+++ src/ViewModels/ViewModels.Workspace/Core/SettingsViewModel/SettingsViewModel.cs
@@ -1,10 +1,15 @@
 ﻿// Copyright (c) Richasy. All rights reserved.
 
+using System;
 using System.Collections.ObjectModel;
+using System.Threading.Tasks;
+using Bili.Models.App.Constants;
 using Bili.Models.Enums;
 using Bili.Models.Enums.Workspace;
 using Bili.Toolkit.Interfaces;
+using Bili.ViewModels.Interfaces.Core;
 using Bili.ViewModels.Interfaces.Workspace;
+using Windows.ApplicationModel;
 
 namespace Bili.ViewModels.Workspace.Core
 {
@@ -18,10 +23,14 @@ namespace Bili.ViewModels.Workspace.Core
         /// </summary>
         public SettingsViewModel(
             ISettingsToolkit settingsToolkit,
-            IAppToolkit appToolkit)
+            IResourceToolkit resourceToolkit,
+            IAppToolkit appToolkit,
+            ICallerViewModel callerViewModel)
         {
             _settingsToolkit = settingsToolkit;
+            _resourceToolkit = resourceToolkit;
             _appToolkit = appToolkit;
+            _callerViewModel = callerViewModel;
             StartupPositions = new ObservableCollection<StartupPosition>
             {
                 StartupPosition.TopLeft,
@@ -46,6 +55,55 @@ namespace Bili.ViewModels.Workspace.Core
             Version = _appToolkit.GetPackageVersion();
             StartupPosition = _settingsToolkit.ReadLocalSetting(SettingNames.StartupPosition, StartupPosition.BottomCenter);
             LaunchType = _settingsToolkit.ReadLocalSetting(SettingNames.LaunchType, LaunchType.Web);
+            StartupInitAsync();
+        }
+
+        private async void StartupInitAsync()
+        {
+            var task = await StartupTask.GetAsync(AppConstants.WorkspaceStartupTaskId);
+            IsStartup = task.State.ToString().Contains("enable", StringComparison.OrdinalIgnoreCase);
+        }
+
+        /// <summary>
+        /// 尝试设置应用自启动.
+        /// </summary>
+        /// <returns><see cref="Task"/>.</returns>
+        private async Task TrySetStartupAsync()
+        {
+            var task = await StartupTask.GetAsync(AppConstants.WorkspaceStartupTaskId);
+            if (IsStartup)
+            {
+                var startupWarningText = string.Empty;
+                if (!task.State.ToString().Contains("enable", StringComparison.OrdinalIgnoreCase))
+                {
+                    var result = await task.RequestEnableAsync();
+                    if (result != StartupTaskState.Enabled)
+                    {
+                        switch (result)
+                        {
+                            case StartupTaskState.DisabledByUser:
+                                startupWarningText = _resourceToolkit.GetLocaleString(LanguageNames.StartupDisabledByUser);
+                                break;
+                            case StartupTaskState.DisabledByPolicy:
+                                startupWarningText = _resourceToolkit.GetLocaleString(LanguageNames.StartupDisabledByPolicy);
+                                break;
+                            default:
+                                break;
+                        }
+
+                        IsStartup = false;
+                    }
+                }
+
+                if (!string.IsNullOrEmpty(startupWarningText))
+                {
+                    _callerViewModel.ShowTip(startupWarningText);
+                }
+            }
+            else
+            {
+                task.Disable();
+            }
         }
 
         partial void OnStartupPositionChanged(StartupPosition value)
@@ -53,5 +111,8 @@ namespace Bili.ViewModels.Workspace.Core
 
         partial void OnLaunchTypeChanged(LaunchType value)
             => _settingsToolkit.WriteLocalSetting(SettingNames.LaunchType, value);
+
+        async partial void OnIsStartupChanged(bool value)
+            => await TrySetStartupAsync();
     }
 }
---
+++ src/Workspace/Controls/Settings/StartupSettingSection.xaml.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) Richasy. All rights reserved.
+
+namespace Bili.Workspace.Controls.Settings
+{
+    /// <summary>
+    /// 开机启动设置区块.
+    /// </summary>
+    public sealed partial class StartupSettingSection : SettingSectionBase
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="StartupSettingSection"/> class.
+        /// </summary>
+        public StartupSettingSection()
+        {
+            InitializeComponent();
+        }
+    }
+}
